e81e1d41309034fe070d8945f1725357fae31614
Hector Hugo Ccasani <hccasani.hc@gmail.com>
2020-08-23 22:23:54 -0500

    beans configuration with annotations

---
+++ src/main/java/pe/example/spring5/configuration/AppConfig.java
@@ -0,0 +1,10 @@
+package pe.example.spring5.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "pe.example.spring5")
+public class AppConfig {
+
+}
